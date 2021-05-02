5044f865ee8e3e05e772852224a80a4e9cfa06a1
Harpreet93 <harpreet2000331@gmail.com>
2021-05-02 16:35:56 -0400

    added new elements between other elements, dom maniuplation

--- Scripts/app.js
+++ Scripts/app.js
@@ -11,6 +11,9 @@
             h2.style.color = "black" ;
             }
     
+
+
+
     function start() 
     {
         console.log("App Started");
@@ -32,16 +35,35 @@
         h2.addEventListener("click", () =>
           {  h2.style.color = (h2.style.color == "green") ? "black" : "green"; }
         );
+        window.addEventListener("mouseover", MouseOver);
+        window.addEventListener("mouseout", MouseOut);
 
+        //create new element between two elements
+        //create new element 
+        let  firstParagraph= document.createElement("p");
+        // config the element 
+        firstParagraph.textContent ="This is a paragraph";
+        //first to do this u have to define paranet node 
+        let parentDiv = h2.parentNode;
+        //insert before h2
+        parentDiv.insertBefore(firstParagraph, h2);
+      
+        //copying the value of one element to the other element 
+         //refrence to h1 
+         let h1 = document.getElementsByTagName("h1")[0];
+        let anotherparagraph = firstParagraph.cloneNode(true);
+        h1.parentNode.insertBefore(anotherparagraph, h1);
+
+        // removing nodes  and hiding them using for, foreach, forin loops
      
-    window.addEventListener("mouseover", MouseOver);
-    window.addEventListener("mouseout", MouseOut);
+     
+  
 
       
     }
 
 
-
+  
 
 
 
